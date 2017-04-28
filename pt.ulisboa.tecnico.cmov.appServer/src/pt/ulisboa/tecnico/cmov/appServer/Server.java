26b5a3447ba6544d865e00d6dcf933ad57fa5ff8
ist-joaof <joao.fonseca@ist.utl.pt>
2017-04-28 19:24:57 +0100

    initial configuration

--- pt.ulisboa.tecnico.cmov.appServer/src/pt/ulisboa/tecnico/cmov/appServer/Server.java
+++ pt.ulisboa.tecnico.cmov.appServer/src/pt/ulisboa/tecnico/cmov/appServer/Server.java
@@ -4,4 +4,24 @@ import java.net.*;
 
 public class Server {
 
+	public static void main(String[] args) {
+
+        int nreq = 1;
+        try
+        {
+            ServerSocket sock = new ServerSocket (8080);
+            for (;;)
+            {
+                Socket newsock = sock.accept();
+                System.out.println("Creating thread ...");
+                Thread t = new ThreadHandler(newsock,nreq);
+                t.start();
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("IO error " + e);
+        }
+        System.out.println("End!");
+    }
 }
